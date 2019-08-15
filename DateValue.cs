4f732c2ebd19b816718b37c6876bd8626870ab70
netextensions <netextensions.io@gmail.com>
2019-08-15 15:46:25 +0200

    Add datevalue named constructor

---
+++ DateValue.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Globalization;
+using LanguageExt;
+using NetExtensions.ValueObject.Legacy;
+
+namespace NetExtensions.ValueObjects.Legacy
+{
+    public class DateValue : ValueObject<DateValue>
+    {
+       private readonly Option<DateTime> _value;
+
+       private DateValue()
+        {
+            _value = Option<DateTime>.None;
+        }
+
+        private DateValue(DateTime dateTime)
+        {
+            _value = dateTime;
+        }
+
+        public Option<DateTime> Value => _value.Match(dt=> dt.Date, () => Option<DateTime>.None);
+
+        public static DateValue Create(DateTime dateTime)
+        {
+            return new DateValue(dateTime);
+        }
+        
+        public static DateValue Create(DateTime? dateTime)
+        {
+            return dateTime.Match(dt => new DateValue(dt), () => new DateValue());
+        }
+
+        public static DateValue Create(string dateTimeString, bool lastCentury = false)
+        {
+            const string yyyyMMddhhssmm = "yyyyMMddhhssmm";
+            const string yyyyMMdd = "yyyyMMdd";
+            const string yyMMdd = "yyMMdd";
+            
+            if (string.IsNullOrEmpty(dateTimeString))
+                return new DateValue();
+
+            var stringDate = dateTimeString.Trim();
+            if (DateTime.TryParseExact(stringDate, new[] {yyyyMMddhhssmm, yyyyMMdd}, null, DateTimeStyles.None, out var date))
+                return new DateValue(date);
+
+            if (!DateTime.TryParseExact(stringDate, yyMMdd, null, DateTimeStyles.None, out var lastCenturyDate))
+                throw new InvalidCastException($"given string is invalid: {dateTimeString}");
+            
+            // do not use TwoDigitYearMax!
+            if (Math.Floor((decimal)lastCenturyDate.Year/100) >= Math.Floor((decimal)DateTime.Now.Year/100))
+            {
+                return lastCentury ? new DateValue(lastCenturyDate.AddYears(-100)) : new DateValue(lastCenturyDate);
+            }
+            return lastCentury ? new DateValue(lastCenturyDate) : new DateValue(lastCenturyDate.AddYears(100));
+        }
+
+        protected override bool EqualsCustom(DateValue other)
+        {
+            if (other == null)
+                return false;
+
+            return other.Value == Value;
+        }
+
+        protected override int GetHashCodeCustom()
+        {
+            return Value.GetHashCode();
+        }
+    }
+}
---
+++ Test/DateValueEqualityTest.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using NetExtensions.ValueObjects.Legacy;
+using NUnit.Framework;
+
+namespace Test
+{
+    [TestFixture]
+    public class DateValueEqualityTest
+    {
+        
+        [TestCase("")]
+        [TestCase("20180518")]
+        [TestCase("19000101")]
+        [TestCase("20401230")]
+        public void TestEquality(string stringValue)
+        {
+            var eq1 = DateValue.Create(stringValue);
+            var eq2 = DateValue.Create(stringValue);
+            Assert.AreEqual(eq1, eq2);
+            Assert.AreEqual(eq1.GetHashCode(), eq2.GetHashCode());
+        }
+
+        [TestCase("20180101010100", "20180101110100")]
+        [TestCase("19211212110142", "19211212082023")]
+        public void TestEqualityYear(string first, string second)
+        {
+            var eq1 = DateValue.Create(first);
+            var eq2 = DateValue.Create(second);
+            Assert.AreEqual(eq1, eq2);
+            Assert.AreEqual(eq1.GetHashCode(), eq2.GetHashCode());
+        }
+
+        [Test]
+        public void TestEqualityDateTime()
+        {
+            var first = DateTime.Now;
+            var second = DateTime.Now;
+            var eq1 = DateValue.Create(first);
+            var eq2 = DateValue.Create(second);
+            Assert.AreEqual(eq1, eq2);
+            Assert.AreEqual(eq1.GetHashCode(), eq2.GetHashCode());
+        }
+        
+        [Test]
+        public void TestEqualityDateTimeNullable_Empty()
+        {
+            DateTime? first = null;
+            DateTime? second = null;
+            var eq1 = DateValue.Create(first);
+            var eq2 = DateValue.Create(second);
+            Assert.AreEqual(eq1, eq2);
+            Assert.AreEqual(eq1.GetHashCode(), eq2.GetHashCode());
+        }
+        
+        [Test]
+        public void TestEqualityDateTimeNullable_NotEmpty()
+        {
+            DateTime? first = DateTime.Now;
+            DateTime? second = DateTime.Now;;
+            var eq1 = DateValue.Create(first);
+            var eq2 = DateValue.Create(second);
+            Assert.AreEqual(eq1, eq2);
+            Assert.AreEqual(eq1.GetHashCode(), eq2.GetHashCode());
+        }
+        
+        [TestCase("")]
+        [TestCase("180518")]
+        [TestCase("000101")]
+        [TestCase("401230")]
+        [TestCase("991230")]
+        public void TestEqualityCentury_Previous(string stringValue)
+        {
+            var eq1 = DateValue.Create(stringValue, true);
+            var eq2 = DateValue.Create(stringValue, true);
+            Assert.AreEqual(eq1, eq2);
+            Assert.AreEqual(eq1.GetHashCode(), eq2.GetHashCode());
+        }
+        
+    }
+}
