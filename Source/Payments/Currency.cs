83c85c04214241b1a9f7005b165842cae76f2da4
netextensions <netextensions.io@gmail.com>
2019-08-23 20:07:52 +0200

    add money

---
+++ Source/Payments/Currency.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using LanguageExt;
+using NetExtensions.ValueObject.Legacy;
+
+namespace NetExtensions.ValueObjects.Legacy.Payments
+{
+    public class Currency : ValueObject<Currency>
+    { 
+        private readonly string _name;
+        private readonly string _base;
+        private readonly string _subUnit;
+        private readonly Option<int> _code;
+
+        private Currency(string name, string @base, string subUnit, Option<int> code)
+        {
+            _base = @base;
+            _subUnit = subUnit;
+            _code = code;
+            _name = name;
+        }
+
+        public Currency Create(string name, string @base, string subUnit)
+        {
+            return new Currency(name, @base, subUnit, Option<int>.None);
+        }
+        
+        public Currency Create(string name, string @base, string subUnit, int code)
+        {
+            return new Currency(name, @base, subUnit, code);
+        }
+        protected override bool EqualsCustom(Currency other)
+        {
+            if (other == null)
+                return false;
+            
+            return GetHashCode() == other.GetHashCode();
+        }
+
+        protected override int GetHashCodeCustom()
+        {
+            return (_name + _base + _subUnit).GetHashCode();
+        }
+    }
+}
---
+++ Source/Payments/Money.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using LanguageExt;
+using NetExtensions.ValueObject.Legacy;
+
+
+namespace NetExtensions.ValueObjects.Legacy.Payments
+{
+    public class Money : ValueObject<Money>
+    {
+        private readonly decimal value;
+        private readonly Currency currency;
+
+        private Money(decimal value)
+        {
+            this.value = value;
+        }
+
+        protected override bool EqualsCustom(Money other)
+        {
+            if (other == null)
+                return false;
+
+            return value == other.value;
+        }
+
+        protected override int GetHashCodeCustom()
+        {
+            return value.GetHashCode();
+        }
+    }
+}
